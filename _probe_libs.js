3e11d55758b889f224ec0b4bccc29392b9091823
linzw <linzw@example.com>
2026-09-01 14:48:11 +0800

    移除探测脚本

--- _probe_libs.js
+++
@@ -1,4 +0,0 @@
-// 探测可用的 libsodium 加密库
-try { require('tweetnacl'); console.log('tweetnacl: yes'); } catch (e) { console.log('tweetnacl: no'); }
-try { require('libsodium-wrappers'); console.log('libsodium-wrappers: yes'); } catch (e) { console.log('libsodium-wrappers: no'); }
-try { require('@actions/core'); console.log('@actions/core: yes'); } catch (e) { console.log('@actions/core: no'); }
